3595a0f6688f141bfc347dbd8af8630d80444a10
freefiremax <67188362+freefiremax@users.noreply.github.com>
2022-05-17 11:34:29 +0530

    Update core.py

--- core.py
+++ core.py
@@ -1,8 +1,8 @@
 #Bot Token From @BotFather
-bot_token = "Bot Token Here"
+bot_token = "5345373810:AAGsv2QSeSqw2m5EGfHABIr2u2W8Etyx-1o"
 
 #Admins Telegram ID
-admins = [1468386562,1914289455]
+admins = [5081665130]
 
 #MongoDb Url For Databse
-mongo_url = "Mongo Url"
+mongo_url = "mongodb+srv://Pythonmohan:pythonmohan@cluster0.s81lb.mongodb.net/?retryWrites=true&w=majority"
